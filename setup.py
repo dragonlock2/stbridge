ad72e2c49d819217ed3baee22932f78f7dce1c50
Matthew Tran <matthewlamtran@berkeley.edu>
2020-11-22 21:34:43 -0800

    Using to pure libusb driver instead

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.extension import Extension
 
 ext = Extension(
     'stbridge',
-    sources=['stbridge.cpp', 'src/bridge/bridge.cpp', 'src/common/criticalsectionlock.cpp', 'src/common/stlink_device.cpp', 'src/common/stlink_interface.cpp', 'src/error/ErrLog.cpp'],
+    sources=['stbridge.cpp', 'libs/src/bridge/bridge.cpp', 'libs/src/common/criticalsectionlock.cpp',
+    	'libs/src/common/stlink_device.cpp', 'libs/src/common/stlink_interface.cpp', 'libs/src/error/ErrLog.cpp'],
     libraries=['boost_python39'],
-    extra_compile_args=['-std=c++17', '-Isrc/bridge', '-Isrc/common', '-Isrc/error'],
+    extra_compile_args=['-std=c++17', '-Ilibs/src/bridge', '-Ilibs/src/common', '-Ilibs/src/error'],
     extra_link_args=['-L.', '-lusb-1.0', '-lfmt']
 )
 
